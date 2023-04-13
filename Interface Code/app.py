8ff4e136e66bd725486a8b8a3d430803679a937f
Imbadimasa <chalganovdima@gmail.com>
2023-04-12 21:04:33 -0400

    added delete function

--- Interface Code/app.py
+++ Interface Code/app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 symbols= []
 portfolio_name = ""
 menu = ""
+run_analyses = False
+
+
 
 @app.route("/")
 def index():
@@ -19,16 +22,28 @@ def index():
 @app.route ("/my_portfolios", methods = ["POST", "GET"])
 def tickers():
     
-    
     ticker = request.form.get("ticker")
     portfolio_name = request.form.get("portfolio_name")
+    timeframe = request.form.get("timeframe")
+    if 
+    
     if request.form.get("View Portfolio") == "View Portfolio":
-        return render_template("my_portfolios.html", symbols=symbols, portfolio_name = portfolio_name)
-    if not ticker and request.form.get("View Portfolio") != "View Portfolio":
+        return render_template("my_portfolios.html", symbols=symbols, portfolio_name = portfolio_name, timeframe = timeframe)
+    
+    if request.form.get("Run Analyses") == "Run Analyses":
+        run_analyses = True
+        return render_template("my_portfolios.html", symbols=symbols, portfolio_name = portfolio_name, run_analyses = True, timeframe = timeframe)
+    
+    if not ticker and request.form.get("View Portfolio") != "View Portfolio" and request.form.get("Run Analyses") != "Run Analyses" and request.form.get("Delete Ticker") != "Delete Ticker":
         return "failure"
+    
+    if request.form.get("Delete Ticker") == "Delete Ticker":
+        symbols.remove(ticker)
+        return render_template("my_portfolios.html", symbols=symbols, portfolio_name = portfolio_name, timeframe = timeframe)
+    
     if request.form.get("View Portfolio") != "View Portfolio":                 
         symbols.append(ticker)
-        return render_template("my_portfolios.html", symbols=symbols, portfolio_name = portfolio_name)
+        return render_template("my_portfolios.html", symbols=symbols, portfolio_name = portfolio_name, timeframe = timeframe)
 
 
 
@@ -54,16 +69,3 @@ def account_manager():
     #TO DO
     return render_template("account_manager.html")
 
-    # if request.form.get("main_menu") == "menu0":
-    #     return render_template ("historical_investment_data.html")
-    # elif request.form.get("main_menu") == "menu1":
-    #     return render_template("current_investment_data.html")
-    # elif request.form.get("main_menu") == "menu2":
-    #     return render_template("portfolio_planner.html")
-    # elif request.form.get("main_menu") == "menu3":
-    #     return render_template("account_manager.html")
-    
-
-    # return render_template("plan_portfolio.html", menu = request.form)
-    
-    #action ="/my_portfolios"
